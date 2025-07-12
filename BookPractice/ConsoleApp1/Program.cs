09e059cce4cfa05eafa048c0297cf78255a30f1d
okutagawa <diziek182@gmail.com>
2025-07-13 02:41:43 +0400

    Приветствие в консольке + навел порядок

---
+++ BookPractice/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = "Знакомство в консоли";
+
+            string name;
+
+            Console.WriteLine("Привет, как тебя зовут?");
+
+            name = Console.ReadLine();
+
+            Console.WriteLine($"Приятно познакомиться, {name}!");
+            Console.WriteLine("Знакомство состоялось!");
+        }
+    }
+}
