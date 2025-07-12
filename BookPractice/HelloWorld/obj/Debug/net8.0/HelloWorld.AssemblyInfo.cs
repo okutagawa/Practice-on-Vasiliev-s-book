a264faaefda19425f4950ad69b44693d3080dffb
okutagawa <diziek182@gmail.com>
2025-07-13 02:51:55 +0400

    123

--- BookPractice/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ BookPractice/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ff2830112677428ca7ecb8206481b795509d367")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cd027ba164c9df15559b03e656de16f8e81f37d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
