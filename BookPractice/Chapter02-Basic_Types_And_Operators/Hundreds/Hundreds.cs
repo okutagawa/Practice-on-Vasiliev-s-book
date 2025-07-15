8266bdae02224afc69d42f7eb10b37a25c857dd4
okutagawa <diziek182@gmail.com>
2025-07-16 03:14:59 +0400

    Примеры программ второй части

---
+++ BookPractice/Chapter02-Basic_Types_And_Operators/Hundreds/Hundreds.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BookPractice;
+
+// Пример программы на нахождение сотен
+
+class Hundreds
+{
+    static void Main()
+    {
+        Console.Write("Введите желаемое число: ");
+        int a = int.Parse(Console.ReadLine());
+
+        int hundreds = a / 100 % 10;
+
+        Console.WriteLine($"В данном числе {hundreds} сотен!");
+    }
+}
---
+++ BookPractice/Chapter02-Basic_Types_And_Operators/OddEven/OddEven.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BookPractice;
+
+// Пример программы на нахождение четного или нечетного числа
+
+class OddEven
+{
+    static void Main()
+    {
+        Console.Write("Введите желаемое число: ");
+        int a = int.Parse(Console.ReadLine());
+
+        string result = (a % 2 == 0) ? "четное" : "нечетное";
+
+        Console.WriteLine($"Вы ввели {result} число!");
+     }
+}
