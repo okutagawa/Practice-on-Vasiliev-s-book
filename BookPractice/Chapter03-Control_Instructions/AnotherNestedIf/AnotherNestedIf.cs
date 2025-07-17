60262910ac7808af7340862cdf47135328139f26
okutagawa <diziek182@gmail.com>
2025-07-17 18:12:47 +0400

    Примеры задач с вложенными и обычными условными операторами

---
+++ BookPractice/Chapter03-Control_Instructions/AnotherNestedIf/AnotherNestedIf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BookPractice;
+
+class AnotherNestedIf
+{
+    static void Main()
+    {
+        Console.Write("Введите целое число: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n == 1) Console.WriteLine("Единица");
+        else if (n == 2) Console.WriteLine("Двойка");
+        else if (n == 3) Console.WriteLine("Тройка");
+        else if (n == 4) Console.WriteLine("Четверка");
+        else if (n == 5) Console.WriteLine("Пятерка");
+        else Console.WriteLine("Неизвестное число");
+        
+    }
+}
---
+++ BookPractice/Chapter03-Control_Instructions/NestedIf/NestedIf.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BookPractice;
+
+class NestedIf
+{
+    static void Main()
+    {
+        Console.Write("Введите текст: ");
+
+        string txt = Console.ReadLine();
+
+        if (txt != "")
+        {
+            Console.WriteLine("Спасибо, что ввели текст!");
+
+            if (txt.Length > 10)
+            {
+                Console.WriteLine("Ого, как много букв!!!");
+            }
+            else
+            {
+                Console.WriteLine("Как-то маловато букв(");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Вы не ввели текст(((");
+        }
+    }
+}
