a37a1b3b4f7f7ffa5b2df088bd9c08c4d6b1289c
okutagawa <diziek182@gmail.com>
2025-07-17 20:01:08 +0400

    switch/anotherswitch

---
+++ BookPractice/Chapter03-Control_Instructions/AnotherNestedIf/Switch/Switch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BookPractice;
+
+class Switch
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+
+        int num = int.Parse(Console.ReadLine());
+
+        switch (num)
+        {
+            case 1:
+                Console.WriteLine("Единица");
+                break;
+            case 2:
+                Console.WriteLine("Двойка");
+                break;
+            case 3:
+                Console.WriteLine("Тройка");
+                break;
+            case 4:
+                Console.WriteLine("Четверка");
+                break;
+            case 5:
+                Console.WriteLine("Пятерка");
+                break;
+            default:
+                Console.WriteLine("Неизвестное число");
+                break;
+        }
+    }
+}
---
+++ BookPractice/Chapter03-Control_Instructions/AnotherSwitch/AnotherSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BookPractice;
+
+class AnotherSwitch
+{
+    static void Main()
+    {
+        Console.Write("Введите целое число от 1 до 9: ");
+
+        int num = int.Parse(Console.ReadLine());
+
+        switch (num)
+        {
+            case 1:
+            case 9:
+                Console.WriteLine("Вы ввели нечетное,\n но не простоое число.");
+                break;
+            case 2:
+            case 3:
+            case 5:
+            case 7:
+                Console.WriteLine("Вы ввели простое число.");
+                break;
+            case 4:
+            case 8:
+                Console.WriteLine("Вы ввели число - степень двойки");
+                break;
+            case 6:
+                Console.WriteLine("Вы ввели 6 - совершенное число");
+                break;
+        }
+    }
+}
