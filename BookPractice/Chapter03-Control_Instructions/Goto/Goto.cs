3d751bb7436eb69491473ff6cf9c3ab78040b7c7
okutagawa <diziek182@gmail.com>
2025-07-19 01:30:16 +0400

    Goto

---
+++ BookPractice/Chapter03-Control_Instructions/Goto/Goto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BookPractice;
+
+class Goto
+{
+    static void Main()
+    {
+        int n = 10, k = 1, s = 0;
+
+        Console.Write("Сумма 1 + 3 + 5 +...+ {0} = ", 2 * n - 1);
+
+    mylabel:
+
+        s += 2 * k - 1;
+        k++;
+
+        if (k <= n) goto mylabel;
+
+
+        Console.WriteLine(s);
+    }
+}
