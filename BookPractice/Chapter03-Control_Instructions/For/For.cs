429dca98cac9f856ee1e37e1f36d1b828a15c343
okutagawa <diziek182@gmail.com>
2025-07-19 01:20:28 +0400

    for

---
+++ BookPractice/Chapter03-Control_Instructions/For/For.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BookPractice;
+
+class For
+{
+    static void Main()
+    {
+        int n = 10;
+
+        int k;
+
+        int s = 0;
+
+        Console.Write("Сумма 1 + 3 + 5 +...+ {0} = ", 2 * n - 1);
+        
+        for (k = 1; k <= n; k++)
+        {
+            s += 2 * k - 1;
+        }
+        Console.WriteLine(s);
+    }
+}
---
+++ BookPractice/Chapter03-Control_Instructions/ForFour/ForFour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BookPractice;
+
+class ForFour
+{
+    static void Main()
+    {
+        int n = 10, k = 1, s = 0;
+
+        Console.Write("Сумма 1 + 3 + 5 +...+ {0} = ", 2 * n - 1);
+
+        for (; k<=n;)
+        {
+            s += 2 * k - 1;
+            k++;
+        }
+        Console.WriteLine(s);
+    }
+}
