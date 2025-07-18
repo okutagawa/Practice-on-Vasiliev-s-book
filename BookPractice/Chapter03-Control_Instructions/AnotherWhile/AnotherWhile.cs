744bc275f2cc9122997cde082a2242ed8692a2c1
okutagawa <diziek182@gmail.com>
2025-07-18 18:14:03 +0400

    while/do-while

---
+++ BookPractice/Chapter03-Control_Instructions/AnotherWhile/AnotherWhile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BookPractice;
+
+class AnotherWhile
+{
+    static void Main()
+    {
+        int n = 10;
+
+        int s = 0;
+
+        Console.Write("Сумма 1 + 3 + 5 +...+ {0} = ", 2 * n - 1);
+
+        while (n>0)
+        {
+            s += 2 * n - 1;
+            n--;
+        }
+        Console.WriteLine(s);
+    }
+}
