d791a914af30ba96fde5f4222057745101df3852
okutagawa <diziek182@gmail.com>
2025-07-20 00:43:44 +0400

    Tasks6-10

---
+++ BookPractice/Independent-work-03/Task7/Task7.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.Intrinsics.Arm;
+
+namespace BookPractice;
+
+// Задание 7. Напишите программу для вычисления суммы квадратов
+// натуральных чисел. Предложите версии программы, использующие разные
+// операторы цикла.
+
+class Task7
+{
+    static void Main()
+    {
+
+        int n = 10;
+        int k = 1;
+        int s = 0;
+
+        Console.Write("Сумма квадратов 1^2 + 2^2 + 3^2 +...+ {0}^2 = ", n);
+
+
+        // Решение через while
+        while (k <= n)
+        {
+            s += k * k;
+            k++;
+        }
+        Console.WriteLine(s);
+
+
+        // Решение через do-while 
+        do
+        {
+            s += k * k;
+            k++;
+        }
+        while (k <= n);
+        Console.WriteLine(s);
+
+
+        // Решение через for 
+        for (k = 1; k <= n; k++) 
+        {
+            s += k * k;
+        }
+        Console.WriteLine(s);
+    }
+}
---
+++ BookPractice/Independent-work-03/Task9/Task9.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.Intrinsics.Arm;
+
+namespace BookPractice;
+
+// Задание 9. Напишите программу, в которой пользователем вводится два целых
+// числа.Программа выводит все целые числа — начиная с наименьшего
+// (из двух введенных чисел) и заканчивая наибольшим(из двух введенных чисел).
+// Предложите разные версии программы(с использованием разных операторов цикла),
+// а также механизм обработки исключений для этой программы.
+
+class Task9
+{
+    static void Main()
+    {
+        Console.Write("Введите первое число: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        int b = int.Parse(Console.ReadLine());
+        int count = 0;
+        int s = 0;
+
+        if (a > b)
+        {
+            // Решение через while
+
+            while (b <= a)
+            {
+                s = b;
+                b += 1;
+                Console.Write(s + " ");
+                count++;
+            }
+
+
+            // Решение через for
+
+            for (count = 0; b <= a; count++)
+            {
+                s = b;
+                b += 1;
+                Console.Write(s + " ");
+            }
+        }
+        else if (a < b)
+        {
+            // Решение через while
+
+            while (a <= b)
+            {
+                s = a;
+                a += 1;
+                Console.Write(s + " ");
+                count++;
+            }
+
+
+            // Решение через for
+
+            for (count = 0; a <= b; count++)
+            {
+                s = a;
+                a += 1;
+                Console.Write(s + " ");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Введите число!");
+        }
+    }
+}
