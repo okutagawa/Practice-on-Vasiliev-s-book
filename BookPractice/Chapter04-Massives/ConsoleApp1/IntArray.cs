90ff92569ca75bab524d68bcd4184d84c9de4d6e
okutagawa <diziek182@gmail.com>
2025-08-05 04:06:34 +0400

    learning massives

---
+++ BookPractice/Chapter04-Massives/ConsoleApp1/IntArray.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BookPractice;
+
+class IntArray
+{
+    static void Main()
+    {
+        int[] nums = new int[12];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = i*3+1; // формула для вычисления чисел, которые при делении на число А дают в остатке число Б
+            Console.Write("| " + nums[i] + " ");
+        }
+        Console.WriteLine("|");
+    }
+}
