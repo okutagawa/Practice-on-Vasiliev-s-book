25cabcc7923e60a58b37d4b6f96be6ea865d71a6
okutagawa <diziek182@gmail.com>
2025-08-07 03:10:17 +0400

    TwoDimArray/MultiDimArray

---
+++ BookPractice/Chapter04-Massives/MultiDimArray/MultiDimArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BookPractice;
+
+class MultiDimArray
+{
+    static void Main()
+    {
+        // тремерный символьный массив
+        char[,,] symbs = new char[4, 3, 5];
+        // одномерный символьный массив
+        char[] s = { 'б', 'Б', 'b', 'B' };
+
+        // заполнение трехмероного массива и отображение значений его элементов
+        
+        // Цикл по первому индексу
+        for (int i = 0; i < symbs.GetLength(0); i++)
+        {
+            Console.WriteLine(" Слой № {0}: ", i+1);
+            // Цикл по второму индексу
+            for (int j = 0; j < symbs.GetLength(1); j++)
+            {
+                // Цикл по третьему индексу
+                for (int k = 0;  k < symbs.GetLength(2); k++)
+                {
+                    // Присваивание значения элементу массива
+                    symbs[i, j, k] = s[i];
+                    // Изменение элемента в одномерном массиве
+                    s[i]++;
+                    // Отображение значения элемента массива
+                    Console.Write(symbs[i, j, k] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("--------------");
+        }
+    }
+}
---
+++ BookPractice/Chapter04-Massives/TwoDimArray/TwoDimArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BookPractice;
+
+class TwoDimArray
+{
+    static void Main()
+    {
+        // кол-во строк и столбцов в массиве
+        int row = 3, cols = 5;
+
+        // создание двумерного массива
+        int[,] nums = new int[row, cols];
+
+        // значение первого элемента в массиве
+        int value = 1;
+
+        // заполнение и отображение массива. Перебор строк в массиве
+
+        for (int i = 0; i < nums.GetLength(0); i++)
+        {
+            // перебор столбцов в строке
+            for (int j = 0; j < nums.GetLength(1); j++)
+            {
+                // присваивание значения элементу массива
+                nums[i, j] = value;
+                // это будет значение след. элемента
+                value++;
+                // отображение элемента в строке
+                Console.Write(nums[i, j] + "\t");
+            }
+            // переход к новой строке
+            Console.WriteLine();
+        }
+    }
+}
