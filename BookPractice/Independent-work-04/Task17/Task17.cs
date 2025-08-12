274ec29ef0a5a9bd350c393e6fd7c76e62aab3ca
okutagawa <diziek182@gmail.com>
2025-08-13 03:04:11 +0400

    stepik tasks

---
+++ BookPractice/Independent-work-04/Task17/Task17.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+
+namespace BookPractice;
+
+// Задание из Stepik Индекс минимального элемента.
+
+// Дан массив целых чисел.Напиши программу, которая находит индекс минимального
+// элемента массива. Индексация начинается с нуля.
+
+class Task17
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string a = Console.ReadLine();
+
+        int[] nums = new int[n];
+
+        string[] numbers = a.Split(' ');
+
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] = int.Parse(numbers[i]);
+        }
+
+        int min = nums[0]; // объявляем минимум и задаем так что минимум это у нас уже первое число массива
+        int index = 0; // индекс соответствующий положению минимума
+         
+        for (int i = 1; i < nums.Length; i++) // начинаем сравнивать со второго индекса массива
+        {
+            if (nums[i] < min)
+            {
+                min = nums[i]; // обновляем минимум
+                index = i; // обновляем индекс если нашли минимум
+            }
+        }
+        Console.WriteLine(index);
+    }
+}
---
+++ BookPractice/Independent-work-04/Task18/Task18.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Reflection;
+
+namespace BookPractice;
+
+// Задание из Stepik Поменять местами первый минимум и последний максимум.
+
+// Дан массив целых чисел. Напиши программу, которая меняет местами первый минимальный
+// элемент и последний максимальный элемент массива. Индексация начинается с нуля.
+
+class Task18
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string a = Console.ReadLine();
+
+        int[] nums = new int[n];
+
+        string[] numbers = a.Split(' ');
+
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] = int.Parse(numbers[i]);
+        }
+
+        int min = nums[0]; // объявляем две переменные для минимума
+        int index1 = 0;
+        
+        int max = nums[0]; // также делаем для максимума
+        int index2 = 0;
+        for (int i = 1; i < nums.Length; i++) // выполняем поиск минимума и максимума со второго индекса
+        {
+            if (nums[i] < min)
+            {
+                min = nums[i];
+                index1 = i;
+            }
+        }
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > max)
+            {
+                max = nums[i];               
+                index2 = i;
+            }
+            if (nums[i] == max) // используем оператор ==, т.к. при равенстве мы перезаписываем индекс на более позднее вхождение того же максимума
+            {
+                index2 = i; // обновляем только индекс, т.к. само максимальное число уже было найдено
+            }
+        }
+
+        nums[index2] = min; // элементу с индексом максимума присваиваем значение минимума
+        nums[index1] = max; // элементу с индексом минимума присваиваем значение максимума
+        for (int i = 0; i < nums.Length; i++)
+        {
+            Console.Write(nums[i] + " ");
+        }
+        // Console.WriteLine($"Минимум - {min} и индекс минимума {index1}\nМаксимум - {max} и индекс максимума {index2}");
+    }
+}
