d6f8685ab5164d96906590fce8b6296dece4f518
okutagawa <diziek182@gmail.com>
2025-08-18 03:15:27 +0400

    end tasks from stepik

---
+++ BookPractice/Independent-work-04/Task23/Task23.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+
+namespace BookPractice;
+
+// Задание из Stepik Таблица умножения (Пифагора).
+
+// Даны два числа n и m. Создай двумерный массив A[n, m] и
+// заполни его таблицей умножения, где i - порядковый номер элемента в строке,
+// а j - порядковый номер элемента в столбце.
+
+class Task23
+{
+    public static void Main()
+    {
+        string a = Console.ReadLine(); // ввод идет следующий (2 3)
+
+        string[] input = a.Split(' '); // сплитим пробелы
+
+        int n = int.Parse(input[0]); // берем из сплита первый элемент, т.е. 2
+        int m = int.Parse(input[1]); // тут второй элемент т.е. 3
+
+        int[,] nums = new int[n, m]; // делаем из данных элементов двумерный массив
+
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                nums[i, j] = (i+1) * (j+1); // заполняем используя +1, т.к. индексация идет с 0, поэтому и используем +1
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write(nums[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
