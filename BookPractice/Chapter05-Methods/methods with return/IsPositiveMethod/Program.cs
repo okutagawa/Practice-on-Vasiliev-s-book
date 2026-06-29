6d7537f528262331c650c445dbc15a0a6c9a69aa
okutagawa <sasha.izotov.04@bk.ru>
2026-06-29 14:36:26 +0400

    add new branch

---
+++ BookPractice/Chapter05-Methods/methods with return/IsPositiveMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IsPositiveMethod;
+
+public class IsPositiveMethod
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+
+        bool isPositive = IsPositive(a);
+
+        Console.WriteLine(isPositive);
+
+    }
+
+    static bool IsPositive(int a)
+    {
+        return a > 0 ? true : false;
+    }
+
+}
---
+++ BookPractice/Chapter05-Methods/methods with return/Multiply/MultiplyMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Multiply;
+
+public class MultiplyMethod
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        int mult = Multiply(a, b);
+
+        Console.WriteLine(mult);
+    }
+
+    static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+}
