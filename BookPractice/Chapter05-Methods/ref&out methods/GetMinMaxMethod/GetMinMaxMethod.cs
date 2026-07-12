2324744a5aa270448bb7119bc7e82c3fe41569dc
okutagawa <sasha.izotov.04@bk.ru>
2026-07-12 15:52:16 +0400

    Finish practice with methods

---
+++ BookPractice/Chapter05-Methods/ref&out methods/GetMinMaxMethod/GetMinMaxMethod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GetMinMaxMethod;
+
+public class GetMinMaxMethod
+{
+    static void GetMinMax(int a, int b, out int min, out int max)
+    {
+        if (a > b)
+        {
+            max = a;
+            min = b;
+        }
+        else
+        {
+            max = b;
+            min = a;
+        }
+    }
+
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        GetMinMax(a, b, out int min, out int max);
+
+        Console.WriteLine($"Минимум: {min}\nМаксимум: {max}");
+    }
+}
---
+++ BookPractice/Chapter05-Methods/ref&out methods/IncrementMethod/IncrementMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IncrementMethod;
+
+public class IncrementMethod
+{
+    static void ChangeValue(ref int number)
+    {
+        number++;
+    }
+
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(num);
+
+        ChangeValue(ref num);
+
+        Console.WriteLine(num);
+    }
+}
