a0b71659616d19492042a8432aaa2c42cc542e2a
okutagawa <sasha.izotov.04@bk.ru>
2026-07-12 22:27:32 +0400

    recursive method

---
+++ BookPractice/Chapter05-Methods/recursive method/PrintNumbersMethod/PrintNumbersMethod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrintNumbersMethod;
+
+public class PrintNumbersMethod
+{
+    static void Main()
+    {
+        PrintNumbers(5);
+    }
+
+    static void PrintNumbers(int n)
+    {
+        if (n <= 0)
+        {
+            return;
+        }
+
+        Console.WriteLine(n);
+
+        n--;
+
+        PrintNumbers(n);
+
+        Console.WriteLine(n + 1);
+    }
+}
---
+++ BookPractice/Chapter05-Methods/recursive method/SumDigitsMethod/SumDigitsMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PrintNumbersMethod;
+
+public class PrintNumbersMethod
+{
+    static void Main()
+    {
+        int number = SumDigits(123);
+    }
+
+    static int SumDigits(int n)
+    {
+
+    }
+}
