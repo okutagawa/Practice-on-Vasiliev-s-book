4daf9df61f52e703f62b7f544a78e346f96e3af7
okutagawa <sasha.izotov.04@bk.ru>
2026-08-01 09:25:00 +0300

    Add example

---
+++ BookPractice/ListCollection/ListExample/ListExample.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookPractice
+{
+    class ListExample
+    {
+        static void Main()
+        {
+            // стандартное объявление и заполнение списка
+            List<string> socialNetworks = new List<string>();
+
+            socialNetworks.Add("Youtube");
+            socialNetworks.Add("Telegram");
+            socialNetworks.Add("Instagram");
+
+            // List<string> socialNetworks = () { "Youtube", "Telegram", "Instagram" }; // альтернатива объявления списка
+
+            socialNetworks.Insert(1, "Tiktok"); // вставка в список по индексу
+
+            // socialNetworks.Remove("Telegram"); // удаление по элементу
+
+            // socialNetworks.RemoveAt(2); // удаление по индексу
+
+            // перенос содержимого из массива в список
+            string[] socialNetworksArray = { "VK", "Max", "Odnoklassniki" };
+
+            socialNetworks.AddRange(socialNetworksArray);
+
+
+
+            socialNetworks.Sort(); //  сортировка по алфавиту
+
+            foreach (string socialNetwork in socialNetworks)
+            {
+                Console.WriteLine(socialNetwork);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
