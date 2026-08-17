95dd0ff20cb179dbba785664dbcb413f9e2b1731
okutagawa <sasha.izotov.04@bk.ru>
2026-08-17 13:15:41 +0400

    Complete task List collection

---
+++ BookPractice/ListCollection/ListIntersection/ListIntersection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookPractice
+{
+    public class ListIntersection
+    {
+        static void Main()
+        {
+            List<int> firstList = new() { 1, 2, 3, 4 };
+
+            List<int> secondList = new() { 3, 4, 5, 6 };
+
+            List<int> resultList = new List<int>();
+
+            for (int i = 0; i < firstList.Count; i++)
+            {
+                for (int j = 0; j < secondList.Count; j++)
+                {
+                    if (firstList[i] == secondList[j])
+                    {
+                        resultList.Add(secondList[j]);
+                    }
+                }
+            }
+
+            foreach (int res in resultList)
+            {
+                Console.Write(res + " ");
+            }
+        }
+    }
+}
---
+++ BookPractice/ListCollection/ListShopping/ListShopping.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookPractice
+{
+    public class ListShopping
+    {
+        static void Main()
+        {
+            List<string> products = new() {"Помидор", "Картошка", "Чипсы", "Мясо", "Молоко"};
+
+            string input = Console.ReadLine();
+
+            
+            if (products.Contains(input))
+            {
+                Console.WriteLine($"Был удален продукт: {input}");
+                products.Remove(input);
+            }
+            else
+            {
+                Console.WriteLine($"Был добавлен продукт: {input}");
+                products.Add(input);
+            }
+            
+            
+            Console.WriteLine();
+
+            foreach (string product in products)
+            {
+                Console.Write(product + " ");
+            }
+            
+        }
+    }
+}
